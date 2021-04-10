71229d22e4d8f07a0b3e78df546196d342d572f7
harell <harel@loostiger.com>
2021-04-10 12:01:18 +1200

    update docker

--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,7 @@
                 "\n\033[44m\033[37m",
                 "\nBuild all docker images with:",
                 "\n.Rprofile$docker$reset()",
+                "\nSys.sleep(10)",
                 "\n.Rprofile$docker$start()",
                 "\n\033[39m\033[49m",
                 sep="")
